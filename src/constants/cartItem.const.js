b505f7adfc780bf5705c1f6a4d6353754a2e0759
bunny <s.mukeshkr481@gmail.com>
2024-10-15 23:49:40 +0530

    modify image path

--- src/constants/cartItem.const.js
+++ src/constants/cartItem.const.js
@@ -1,17 +1,19 @@
+import productImage from "../assets/hard-disk.jpg";
+
 export const CART_ITEMS_MOCK = [
     {
         id: 1,
-        imgUrl: "/assets/hard-disk.jpg",
+        imgUrl: productImage,
         isSelected: false,
     },
     {
         id: 2,
-        imgUrl: "/assets/hard-disk.jpg",
+        imgUrl: productImage,
         isSelected: false,
     },
     {
         id: 3,
-        imgUrl: "/assets/hard-disk.jpg",
+        imgUrl: productImage,
         isSelected: false,
     },
 ];
